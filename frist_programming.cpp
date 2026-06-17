926fce8d0b47609ed2677c4f96fef9aaa7d68b26
mdsak <mdsakilkhan649@gmail.com>
2026-06-18 03:06:09 +0600

    first commit

---
+++ frist_programming.cpp
@@ -0,0 +1,33 @@
+/*
+#include <iostream>
+using namespace std;// এটি লিখলে বারবার std::cout লিখতে হয় না
+
+int main(){
+
+    int a,b,sum;
+
+    // ১. ইউজারের কাছে সংখ্যা চাওয়া (printf এর বদলে cout)
+    cout << "Enter First number: ";
+    cin >> a;// scanf এর বদলে cin
+
+    cout << "Enter second number: ";
+    cin >> b;
+
+    // ২. যোগফল বের করা (সি ল্যাঙ্গুয়েজের মতোই)
+    sum = a+b;
+
+    // ৩. রেজাল্ট প্রিন্ট করা
+    cout << "The result is: " << sum << endl;
+
+    return 0;
+}
+*/
+#include<iostream>
+using namespace std;
+int main(){
+    int age;
+cout <<"Enter your age:";
+cin >>age;
+cout <<"Your age is:" << age ;
+    return 0;
+}
